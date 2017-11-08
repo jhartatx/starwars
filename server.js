2b086eaf1b589c262abc258b25d02f40ea6d5a64
jhartatx <jhartatx@gmail.com>
2017-11-08 14:01:27 -0600

    all updates

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
